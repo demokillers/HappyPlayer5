3864464d801a08b3a27e2acbf01b81b2c2e3aede
wuzihua <wuzihua@bigo.sg>
2021-12-07 17:27:37 +0800

    歌词库以源码方式引入

--- app/src/main/java/com/zlm/hp/fragment/LrcFragment.java
+++ app/src/main/java/com/zlm/hp/fragment/LrcFragment.java
@@ -190,12 +190,13 @@ public class LrcFragment extends BaseFragment {
                 } else {
                     fileName = mCurAudioInfo.getSingerName() + " - " + mCurAudioInfo.getSongName();
                 }
-                mLrcFilePath = ResourceFileUtil.getFilePath(mActivity.getApplicationContext(), ResourceConstants.PATH_LYRICS, fileName + ".krc");
+                mLrcFilePath = ResourceFileUtil.getFilePath(mActivity.getApplicationContext(), ResourceConstants.PATH_LYRICS,  "join_word_ori.lrc");
+                String text = ResourceFileUtil.getFilePath(mActivity.getApplicationContext(), ResourceConstants.PATH_LYRICS,  "join_word_ori_text.lrc");
 
                 //
                 LyricsReader lyricsReader = new LyricsReader();
                 lyricsReader.setHash(mHash);
-                lyricsReader.loadLrc(mDownloadLyricsResult.getContent(), null, mLrcFilePath);
+                lyricsReader.loadLrc(mDownloadLyricsResult.getContent(), new File(text), mLrcFilePath);
                 mManyLineLyricsView.setTextMaxWidth(mScreensWidth / 3 * 2);
                 mManyLineLyricsView.setLyricsReader(lyricsReader);
                 if (mHPApplication.getPlayStatus() == AudioPlayerManager.PLAYING && mManyLineLyricsView.getLrcStatus() == AbstractLrcView.LRCSTATUS_LRC && mManyLineLyricsView.getLrcPlayerStatus() != AbstractLrcView.LRCPLAYERSTATUS_PLAY)
